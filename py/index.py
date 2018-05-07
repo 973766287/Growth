1457ea119932f167a10da906165f56aa7fb3d700
973766287 <973766287@qq.com>
2018-05-07 10:16:19 +0800

    upload

---
+++ py/index.py
@@ -0,0 +1,23 @@
+# coding=UTF-8 
+# print "Holle Wold!";
+
+# n = 1;x = 2;
+# if n + x == 3:
+#     raw_input('you are right');
+# elif n == 0:
+#     print 'no!';
+# else:
+#     print 'haha';
+
+# raw_input('you are right');
+# bianliang = 'justforfun';
+# # hanzi     = '你好！';
+# print bianliang;
+
+list = ['one','two','three'];
+dect = {'key1':'value1','zone':'0'};
+list[2] = 'four';
+dect['add'] = 'new value';
+print dect; 
+print dect.keys();
+print dect.values();
